370c24450ecb1d0fa5cceade6044ce89cc7a16dd
Roei <roeioren10@gmail.com>
2021-06-17 10:47:29 +0300

    fix mir not having uniqeId

--- src/utils/assembleUniqueID.ts
+++ src/utils/assembleUniqueID.ts
@@ -1,13 +1,8 @@
 import matchedRecordType from '../types/matchedRecord';
 import { DOMAIN_SUFFIXES } from '../config/db_enums';
-import fieldNames from '../config/fieldNames';
 
 const domainSuffixes: Map<string, string> = new Map<string, string>(JSON.parse(JSON.stringify(DOMAIN_SUFFIXES)));
 
 export default (record: matchedRecordType): string => {
-    if (record.source === fieldNames.sources.mir) {
-        return '';
-    }
-
-    return `${record.userID}${domainSuffixes.get(record.source)}`
-}
+    return `${record.userID}${domainSuffixes.get(record.source)}`;
+};
