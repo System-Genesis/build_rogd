485ef96af4543592b2006255cf6a0d6a7349664d
NoamYes <noamiko600@gmail.com>
2021-07-05 11:25:56 +0300

    menahem is a king

--- src/config/db_enums.ts
+++ src/config/db_enums.ts
@@ -1,15 +1,6 @@
 import fieldNames from './fieldNames';
 
-export const DOMAIN_SUFFIXES = [
-    [fieldNames.sources.ads, '@rabiran.com'],
-    [fieldNames.sources.es, '@jello.com'],
-    [fieldNames.sources.adNN, '@adnn.com'],
-    [fieldNames.sources.sf, '@leonardo.com'],
-    [fieldNames.sources.city, '@city.com'],
-    [fieldNames.sources.mir, '@city.com'],
-];
-
-export const ROLE_ID_SUFFIXES = [
+export const ROLE_ID_SUFFIXES :[string, string][]= [
     [fieldNames.sources.ads, '@rabiran'],
     [fieldNames.sources.es, '@jello'],
     [fieldNames.sources.adNN, '@adnn'],
--- src/utils/assembleRoleID.ts
+++ src/utils/assembleRoleID.ts
@@ -1,7 +1,7 @@
 import matchedRecordType from '../types/matchedRecord';
 import { ROLE_ID_SUFFIXES } from '../config/db_enums';
 
-const roleIdSuffixes: Map<string, string> = new Map<string, string>(JSON.parse(JSON.stringify(ROLE_ID_SUFFIXES)));
+const roleIdSuffixes: Map<string, string> = new Map<string, string>(ROLE_ID_SUFFIXES);
 
 export default (record: matchedRecordType): string => {
     return `${record.userID.split('@')[0]}${roleIdSuffixes.get(record.source)}`;
