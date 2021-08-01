4bd81a9533ac68154eebe733e16044fceafc1a0e
Roei <roeioren10@gmail.com>
2021-08-01 10:02:28 +0300

    city is not role attachable

--- src/builds/buildDI.ts
+++ src/builds/buildDI.ts
@@ -5,12 +5,12 @@ import digitalIdentityObj from '../types/digitalIdentity';
 
 export default (record: matchedRecordType, identifier: string) => {
     const digitalIdentity: digitalIdentityObj = {
-        type: record.source === fieldNames.sources.mir || record.source === fieldNames.sources.city ? 'kaki' : 'domUser',
+        type: record.source === fieldNames.sources.mir ? 'kaki' : 'domUser',
         source: record.source,
         mail: record.mail,
         uniqueId: record.userID,
         entityId: identifier,
-        isRoleAttachable: record.source !== fieldNames.sources.mir && record.source !== fieldNames.sources.city,
+        isRoleAttachable: record.source !== fieldNames.sources.mir,
     };
 
     cleanObj(digitalIdentity);
