ac7512c5f21d0638761e0f1b42cc877f7d906d53
Roei <roeioren10@gmail.com>
2021-10-04 12:56:17 +0300

    small fix

--- src/builds/buildRole.ts
+++ src/builds/buildRole.ts
@@ -4,12 +4,10 @@ import assembleRoleID from '../utils/assembleRoleID';
 import { oneTreeSources } from '../config/db_enums';
 import fieldNames from '../config/fieldNames';
 
-export default (record: matchedRecordType, DIUniqueID: string): roleType => {
+export default (record: matchedRecordType): roleType => {
     return {
         roleId: assembleRoleID(record),
         jobTitle: record.job || fieldNames.unknown,
-        digitalIdentityUniqueId: DIUniqueID,
-        hierarchy: record.hierarchy!,
         source: oneTreeSources.includes(record.source) ? fieldNames.sources.oneTree : record.source,
     };
 };
